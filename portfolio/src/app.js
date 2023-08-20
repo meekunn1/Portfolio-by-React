ddea50a7ca1af4acab07bc4f248ef28a3b3622d0
Keiji O <meekunn@gmail.com>
2023-08-20 02:27:59 -0700

    folder organized, npx create-route-app used. index.js and app.js init created.

---
+++ portfolio/src/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Project from "./components/Project";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Project />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
