a1506a63cf568d232d79c951650d04848badf195
Keiji O <meekunn@gmail.com>
2023-10-23 21:53:24 -0700

    cleaned bootstrap for badges.js

--- src/components/badges.js
+++ src/components/badges.js
@@ -1,52 +1,14 @@
 import React, { useEffect } from "react";
 
 const styles = {
-  flexContainer: {
-    display: "flex",
-    flexDirection: "row",
-    flex: "1",
-  },
-  flexItem: {
-    display: "flex",
-    flex: "1",
-  },
-  imageStyle: {
-    fontSize: "50%",
-  },
   marginAll: {
     margin: "10px",
     padding: "10px",
   },
-  center: {
-    height: "25%",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    margin: "10px",
-  },
-  skillList: {
-    width: "60rem",
-    margin: "10px",
-  },
-  primaryListArea: {
-    // background: 'black',
-    width: "30rem",
-    padding: "10px",
-    margin: "10px",
-    borderStyle: "solid",
-    borderColor: "green",
-    // backgroundColor: "#c3ffd5",
-  },
-  secondaryListArea: {
-    // background: 'black',
-    width: "30rem",
-    padding: "10px",
-    margin: "10px",
-    borderStyle: "solid",
-    borderColor: "blue",
-    // backgroundColor: "#ededed",
-  },
 };
+
+const badgeWidth = "350";
+const badgeLength = "270";
 
 export default function Badges() {
   useEffect(() => {
@@ -66,45 +28,45 @@ export default function Badges() {
       <h1>Badges</h1>
       <div className='card' style={styles.marginAll}>
         <p>The following are the Badges I have received on Credly.</p>
-        <div className='card d-flex flex-row flex-wrap align-self-stretch' style={styles.marginAll}>
+        <div className=' d-flex flex-row flex-wrap align-self-stretch justify-content-center align-content-center'>
           <div
           id="HTML&CSS"
-            class='p-2'
-            data-iframe-width='150'
-            data-iframe-height='270'
+            class='x-2'
+            data-iframe-width={badgeWidth}
+            data-iframe-height={badgeLength}
             data-share-badge-id='1dd35a27-0109-4e0a-acd4-4fce63e6041a'
             data-share-badge-host='https://www.credly.com'
           ></div>
           <div
             id='SQL&NoSQL'
-            class='p-2'
-            data-iframe-width='150'
-            data-iframe-height='270'
+            class='x-2'
+            data-iframe-width={badgeWidth}
+            data-iframe-height={badgeLength}
             data-share-badge-id='95ff92a7-4aaf-464b-ab1e-d8988c048eda'
             data-share-badge-host='https://www.credly.com'
           ></div>
 
           <div
             id='Node&Express'
-            class='p-2'
-            data-iframe-width='150'
-            data-iframe-height='270'
+            class='x-2'
+            data-iframe-width={badgeWidth}
+            data-iframe-height={badgeLength}
             data-share-badge-id='b62e6f7f-f9b0-4bc6-bee3-3d9b688a4b1e'
             data-share-badge-host='https://www.credly.com'
           ></div>
           <div
             id='JS&Jquery'
-            class='p-2'
-            data-iframe-width='150'
-            data-iframe-height='270'
+            class='x-2'
+            data-iframe-width={badgeWidth}
+            data-iframe-height={badgeLength}
             data-share-badge-id='0629c411-e069-4c0a-89c9-199b66af2b32'
             data-share-badge-host='https://www.credly.com'
           ></div>
           <div
             id='React'
-            class='p-2'
-            data-iframe-width='150'
-            data-iframe-height='270'
+            class='x-2'
+            data-iframe-width={badgeWidth}
+            data-iframe-height={badgeLength}
             data-share-badge-id='8bbaf684-b09d-48d6-89e6-d5b75e71c01a'
             data-share-badge-host='https://www.credly.com'
           ></div>
